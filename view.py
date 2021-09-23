796f0d74b5fbe46cb3c4cc655fc2acb86a97ea91
kohenro19 <kohenro19@gmail.com>
2021-09-23 11:30:15 +0900

    20210923r1

---
+++ view.py
@@ -0,0 +1,45 @@
+import eel
+from pos_system import Order, Item
+import pandas as pd
+
+ITEM_MASTER_CSV_PATH="./item_master.csv"
+RECEIPT_FOLDER= "receipt"
+
+def add_item_master_by_csv(csv_path):
+        print("------- マスタ登録開始 ---------")
+        item_master=[]
+        count=0
+        try:
+            item_master_df=pd.read_csv(csv_path,dtype={"item_code":object}) # CSVでは先頭の0が削除されるためこれを保持するための設定
+            for item_code,item_name,price in zip(list(item_master_df["item_code"]),list(item_master_df["item_name"]),list(item_master_df["price"])):
+                item_master.append(Item(item_code,item_name,price))
+                print("{}({})".format(item_name,item_code))
+                count+=1
+            print("{}品の登録を完了しました。".format(count))
+            print("------- マスタ登録完了 ---------")
+            return item_master
+        except:
+            print("マスタ登録が失敗しました")
+            print("------- マスタ登録完了 ---------")
+            # sys.exit()
+        
+
+def main():
+    # マスタ登録
+    item_master=add_item_master_by_csv(ITEM_MASTER_CSV_PATH) # CSVからマスタへ登録
+    order=Order(item_master,RECEIPT_FOLDER) 
+
+    # # オーダー登録
+    # order.add_item_order()
+    
+    # # オーダー表示
+    # order.view_item_list()
+
+    eel.init("web")
+    eel.start("index.html")
+
+if __name__ == "__main__":
+    main()
+
+    
+
