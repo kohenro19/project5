c9d32dedc3a3d815a91bbc869db621bf69323096
kohenro19 <kohenro19@gmail.com>
2021-10-19 07:35:58 +0900

    20211019r1

--- pos_system.py
+++ pos_system.py
@@ -5,11 +5,59 @@ import datetime
 import pathlib
 import pandas as pd
 
+class Item():
+    def __init__(self, item_code, item_name, price) -> None:
+        self.item_code = item_code
+        self.item_name = item_name
+        self.item_price = price
+    pass
+
+class Order():
+    def __init__(self, item_master) -> None:
+        self.item_order_list=[]
+        self.item_count_list=[]
+        self.item_master=item_master
+
+    def add_item_order(self, item_code, item_count):
+        if self.get_item_data(item_code):
+            self.item_order_list.append(item_code)
+            self.item_count_list.append(item_count)            
+            return True
+        else:
+            return False
+
+    def get_item_data(self, item_code):
+        for master in self.item_master:
+            if item_code == master.item_code:
+                return master.item_name, master.item_price
+
+    def get_order_items(self):
+        '''
+        オーダーの全情報をテキストをして取得する
+        '''
+        res = ""
+        num = 1
+        total_price = 0
+        total_count = 0
+        for item_code,count in zip(self.item_order_list,self.item_count_list):
+            for item in self.item_master:
+                if item.item_code == item_code:
+                    res += f"{num} | {item_code} {item.item_name} | ￥{item.item_price}円 × {count} 個\n"
+                    num += 1
+                    total_price += item.item_price * count
+                    total_count += count
+                    break   
+        res += "---------------------------------------------\n"
+        res += f"合計: ￥{total_price}円 | {total_count}個\n"
+
+        return res
+        
 
 class PosSystem():
     def __init__(self, item_master_csv) -> None:
         self.item_master = []
         self.item_master_csv = item_master_csv
+        self.order = None
 
     def add_item_master(self):
         print("------- マスタ登録開始 ---------")
@@ -28,11 +76,8 @@ class PosSystem():
             print("------- マスタ登録完了 ---------")
             sys.exit()
         
-class Item():
-    def __init__(self, item_code, item_name, price) -> None:
-        self.item_code = item_code
-        self.item_name = item_name
-        self.item_price = price
-    pass
+    def init_order(self):
+        self.order = Order(self.item_master) 
+    
 if __name__ == "__main__":
     main()
